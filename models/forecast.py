b0a0ef6237014403fcbcaabe0c06bbecd40855ce
bardo-sama <bardopapuashvilik@gmail.com>
2026-07-13 19:00:37 +0300

    Add forecast units field and comments

--- models/forecast.py
+++ models/forecast.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import pandas as pd
 
+# Один об'єкт Forecast зберігає прогноз для конкретної target_date.
 class Forecast:
-    def __init__(self, city_name, source, latitude, longitude, collected_date, target_date, weather_data):
+    def __init__(self, city_name, source, latitude, longitude, collected_date, target_date, weather_data, units):
         self.city_name = city_name
         self.source = source
         self.latitude = latitude
@@ -10,10 +11,12 @@ class Forecast:
         self.collected_date = collected_date
         self.target_date = target_date
         self.weather_data = weather_data
+        self.units = units
 
     def __repr__(self):
         return f'Class: Forecast, collected date: {self.collected_date}, target date: {self.target_date}'
 
+    # Короткий текстовий опис прогнозу для ручної перевірки.
     def show_summary(self):
         print('-' * 21)
         print(f'Джерело: {self.source}')
@@ -23,11 +26,13 @@ class Forecast:
 
     @property
     def lead_days(self):
+        # Різниця між датою прогнозу та датою його отримання.
         collected = datetime.strptime(self.collected_date, "%Y-%m-%d").date()
         target = datetime.strptime(self.target_date, "%Y-%m-%d").date()
 
         return (target - collected).days
 
+    # Перетворюємо об'єкт у словник для подальшого збереження в JSON.
     def to_dict(self):
         obj_datafile = {
             'city_name': self.city_name,
@@ -39,10 +44,12 @@ class Forecast:
             'collected_date': self.collected_date,
             'target_date': self.target_date,
             'lead_days': self.lead_days,
+            'units': self.units,
             'weather_data': self.weather_data.to_dict('records')
         }
         return obj_datafile
 
+    # Відновлюємо Forecast зі словника, завантаженого з JSON.
     @classmethod
     def from_dict(cls, data):
 
@@ -51,6 +58,7 @@ class Forecast:
 
         coordinates = data.get('coordinates', {})
 
+        # Погодні записи з JSON повертаємо назад у DataFrame.
         weather_data = pd.DataFrame(data.get('weather_data', []))
 
         return cls(
@@ -60,6 +68,6 @@ class Forecast:
             longitude=coordinates.get('longitude'),
             collected_date=data.get('collected_date'),
             target_date=data.get('target_date'),
-            weather_data=weather_data
+            weather_data=weather_data,
+            units=data.get('units', {})
         )
-
